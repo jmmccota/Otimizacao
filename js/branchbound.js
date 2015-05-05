7403a7317fe944c02e9edd949508f42fee7a0438
EliasLuiz <eliasluizjr@gmail.com>
2015-05-05 16:05:02 -0300

    Criado estrutura basica do solver

---
+++ js/branchbound.js
@@ -0,0 +1,39 @@
+function BranchBound(/* Parametros do simplex */){
+    /*
+        Classe que controlara a chamada do simplex
+    */
+    
+    /* Parametros do simplex em variaveis privadas*/
+    include("caminho do simplex");
+    include("heap.js");
+    
+    
+    this.passoAPasso = function(){
+        
+    };
+    
+    this.proximoPasso = function(){
+        
+    };
+    
+    this.resolve = function(){
+        
+    };
+    
+    this.getHeap = function(){
+        
+    };
+    
+}
+
+
+
+
+
+
+function include(path){
+    var aux = document.createElement("script");
+    aux.type = "text/javascript";
+    aux.src = path;
+    document.body.appendChild(aux);
+};
