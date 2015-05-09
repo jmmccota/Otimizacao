bd13c33a5edd2261592cc467b4aa483748258b5a
EliasLuiz <eliasluizjr@gmail.com>
2015-05-09 15:41:52 -0300

    Terminado algoritmo branch and bound

--- js/branchbound.js
+++ js/branchbound.js
@@ -22,7 +22,7 @@ Nodo = function (id, pai, altura, modelo, z, x) {
 
     this.toSource = function () {
         var source = "";
-        
+
         source = this.problema + '\n';
         source += "obj: ";
         for (var i = 0; i < this.objetivo.length; i++) {
@@ -32,7 +32,7 @@ Nodo = function (id, pai, altura, modelo, z, x) {
         source += "\n\n" + "Subject To" + "\n";
 
         for (i = 0; i < restricoes.length; i++) {
-            source += "res_" + (i+1) + ":";
+            source += "res_" + (i + 1) + ":";
             for (var j = 0; j < objetivo.length; j++) {
                 source += (this.restricoes[i][j] >= 0) ? " +" : " ";
                 source += this.restricoes[i][j] + " x" + j;
@@ -63,6 +63,10 @@ Nodo = function (id, pai, altura, modelo, z, x) {
     return this;
 };
 
+
+//////////////////////////////////////////////////////////////////////////////
+
+
 Heap = function (nodo) {
     /*
      * Classe que sera usada como "arvore" para o metodo branch and bound.
@@ -73,26 +77,6 @@ Heap = function (nodo) {
      */
     this.array = new Array(0);
     this.array[1] = nodo;
-
-    //EXCLUIR ?????
-    this.insereModelos = function (pai, mEsq, mDir) {
-        /*
-         * pai eh o indice do elemento pai de esq e dir.
-         * esq eh o modelo que sera o filho a esquerda de pai.
-         * se esq for 'null' adiciona somente o elemento a direita.
-         * dir eh o modelo que sera o filho a direita de pai.
-         * se dir for 'null' adiciona somente o elemento a esquerda.
-         */
-        
-        if (mEsq != null){
-            var esq = Nodo(pai*2, pai, array[pai].altura+1, mEsq, 0, 0);
-            this.array[esq.id] = esq;
-        }
-        if (mDir != null){
-            var dir = Nodo(pai*2+1, pai, array[pai].altura+1, mDir, 0, 0);
-            this.array[dir.id] = dir;
-        }
-    };
 
     this.insereNodos = function (pai, esq, dir) {
         /*
@@ -116,112 +100,77 @@ Heap = function (nodo) {
         }
     };
 
-    this.busca = function (nodo) {
-        /*
-         * Retorna o indice em que o objeto 'nodo' esta no heap.
-         * Retorna 0 caso nao esteja.
-         * 
-         * SUBSTITUIR POR INDEX OF ?????
-         * 
-         */
-        for (var i = 1; i < this.array.length; i++)
-            if (i in this.array && this.array[i] === nodo)
-                return i;
-        return 0;
-    };
-
-    this.substitui = function (velho, novo) {
-        /*
-         * Substitui o objeto 'velho' pelo objeto 'novo'.
-         */
-        var indice = this.busca(velho);
-        if (indice !== 0)
-            this.array[indice] = novo;
-    };
-
-    this.getPai = function (nodo) {
-        /*
-         * Nodo e o indice de um elemento
-         * Retorna o indice do pai do elemento
-         */
-        return Math.floor(nodo / 2);
-    };
-
     return this;
 };
+
+
+//////////////////////////////////////////////////////////////////////////////
+
 
 BranchBound = function () {
     /*
      * Classe que controla a chamada do simplex
      * Usa um heap para simular a arvore de possibilidades
      */
+    var modelo = leituraParametros();
 
-
-    this.inicializa = function () {
-        /*
-         * "Construtor" da classe BranchBound
-         * Resolve o simplex para o modelo inicial e retorna o nodo
-         */
-        var modelo = leituraParametros();
-
-        var nodo = Nodo(1, 1, 0, modelo, 0, 0);
-        this.heap = Heap(nodo);
-        this.atual = 1;
-        this.fila = [];
-        
-        var res = simplex(this.heap.array[1]);
-
-        this.heap.array[1].z = res['z'];
-        this.heap.array[1].x = res['x'];
-
-        return this.heap.array[1];
-    };
+    var nodo = Nodo(1, 1, 0, modelo, 0, 0);
+    this.heap = Heap(nodo);
+    this.atual = 1;
+    this.fila = [1];
 
     this.terminou = function () {
         //se a fila de proximo nodo a analisar estiver vazia a execucao terminou
-        return heap.fila === [];
+        return (this.fila.length == undefined || this.fila.length == 0);
     };
 
-    this.proximoPasso = function (xi) {
+    this.proximoPasso = function (escolhaVariavel) {
         /*
-         * xi eh o id do x que sera alterado
+         * escolhaVariavel eh uma funcao para escolher qual xi sofrera a bifurcacao.
+         *     para executar normalmente se passa this.escolheVariavel.
+         *     para executar passo a passo se passa uma funcao que retorna o indice
+         *         do xi que o usuario escolheu.
          * executa uma iteracao do branch and bound:
          *     gera os proximos branchs e resolve o proximo da fila
          *     retorna o nodo que foi resolvido
          */
-        //se heap[atual].x[xi] for fracionario
-        var x = heap.array[atual].x[xi];
-        if (x != Math.floor(x)) {
-            //adiciona restricao de heap[atual].x[xi], gerando 2 modelos
-            if(heap.array[atual].problema == "Maximize")
-                heap.array[atual].z = "-Inf";
+        
+        //retira o 1o da fila
+        nodo = this.heap.array[fila.shift()];
+        //atual = 1o da fila
+        this.atual = nodo.id;
+        //resolve o simplex
+        var res = simplex(nodo);
+        //completa o nodo
+        nodo.x = res["x"];
+        nodo.z = res["z"];
+
+        //escolhe qual variavel vai sair
+        xi = escolhaVariavel();
+
+        //se heap[atual].x[xi] for viavel e fracionario
+        var x = this.heap.array[atual].x[xi];
+        if (!isNaN(this.heap.array[atual].z) &&
+                x !== Math.floor(x)) {
+
+            //gera 2 copias do objeto
+            var esq = jQuery.extend(true, {}, this.heap.array[atual]);
+            var dir = jQuery.extend(true, {}, this.heap.array[atual]);
+
+            //se possui solucao fracionaria altera o valor de Z
+            if (this.heap.array[atual].problema === "Maximize")
+                this.heap.array[atual].z = "-Inf";
             else
-                heap.array[atual].z = "Inf";
-            var esq = jQuery.extend(true, {}, heap.array[atual]);
-            var dir = jQuery.extend(true, {}, heap.array[atual]);
-            
+                this.heap.array[atual].z = "Inf";
+
+            //adiciona restricao de heap[atual].x[xi] nos 2 modelos
             dir.lower[xi] = Math.ceil(x);
             esq.upper[xi] = Math.floor(x);
-            
+
             //insere 2 nodos no heap e na fila
-            heap.insereNodos(atual, esq, dir);
-            fila.push(atual * 2);
-            fila.push(atual * 2 + 1);
-        }
-
-        var nodo = null;
-
-        //se nao terminou
-        if (!terminou()) {
-            //retira o 1o da fila
-            nodo = heap.array[fila.shift()];
-            //atual = 1o da fila
-            atual = nodo.id;
-            //resolve o simplex
-            var res = simplex(nodo);
-            //completa o nodo e retorna
-            nodo.x = res["x"];
-            nodo.z = res["z"];
+            this.heap.insereNodos(atual, esq, dir);
+            this.fila.push(atual * 2);
+            this.fila.push(atual * 2 + 1);
         }
 
         return nodo;
@@ -231,18 +180,26 @@ BranchBound = function () {
         //le variavel q o usuario clicou
         return proximoPasso(/*indice do x q o usuario escolheu*/);
     };
-    
-    this.executar = function (){
-        return proximoPasso(escolheVariavel(heap.array[atual].x));
+
+    this.executar = function () {
+        return proximoPasso(this.escolheVariavel);
     };
 
-    this.escolheVariavel = function (x) {
+    this.escolheVariavel = function () {
         /*
-         * Retorna o indice da variavel mais fracionaria de x
+         * Retorna o indice da variavel mais fracionaria do x do nodo atual
          */
+        x = this.heap.array[this.atual].x;
+        
         var mini = 0;
         var minval = 1;
-        for (var i = 0; i < x.lenght; i++) {
+
+        //x.length retorna undefined
+        len = 0;
+        while (x[len] !== undefined)
+            len++;
+
+        for (var i = 0; i < len; i++) {
             if (x[i] !== Math.floor(x[i])) {
                 //Se for fracionario
                 if (Math.abs(x[i] - Math.floor(x[i]) - 0.5) < minval) {
@@ -253,30 +210,67 @@ BranchBound = function () {
         }
         return mini;
     };
-    
-    this.melhorSolucao = function (){
-        //retornar o nodo com maior/menor Z se numero
-        var otimVal = heap.array[1].z;
-        var otimi = 1;
-        for(var i = 2; i<heap.array.length; i++){
-            if(heap.array[1].problema === 'Maximize' && 
-               !isNaN(heap.array[i].z) &&
-               heap.array[i].z > otimVal){
-                otimVal= heap.array[i].z;
-                otimi = i;
+
+    this.melhorSolucao = function () {
+        /*
+         * retorna o nodo com maior/menor Z
+         * retorna null caso nao haja solucao inteira viavels
+         */
+        /*
+        var otimVal = this.heap.array[1].z;
+        var otim = this.heap.array[1];
+        for (nodo in this.heap.array) {
+            if (otim.problema === 'Maximize' &&
+                    !isNaN(nodo.z) &&
+                    nodo.z > otimVal) {
+                otimVal = nodo.z;
+                otim = nodo;
             }
-            else if(heap.array[1].problema === 'Minimize' &&
-                    !isNaN(heap.array[i].z) &&
-                    heap.array[i].z < otimVal){
-                otimVal= heap.array[i].z;
-                otimi = i;
+            else if (otim.problema === 'Minimize' &&
+                    !isNaN(nodo.z) &&
+                    nodo.z < otimVal) {
+                otimVal = nodo.z;
+                otim = nodo;
             }
         }
-        return heap.array[otimi];
+        if(!isNaN(otim.z))
+            return otim;
+        else
+            return null;
+        */
+        //procura a 1a solucao inteira viavel
+        //    se nao houver nenhuma retorna null
+        //    se encontrar seta como otima
+        var i = 0;
+        while(this.heap.array[i] === undefined ||
+              isNaN(this.heap.array[i].z))
+            i++;
+        if(i > this.heap.array.length)
+            return null;
+        
+        var otim = this.heap.array[i];
+        
+        //para cada solucao
+        for(nodo in this.heap.array){
+            //se solucao eh viavel
+            if(!isNaN(nodo.z)){
+                //e for melhor que a otima
+                if(nodo.problema === "Maximize" && nodo.z > otim.z ||
+                   nodo.problema === "Minimize" && nodo.z < otim.z)
+                    // passa a ser a nova otima
+                    otim = nodo;
+            }
+        }
+        
+        return otim;
     };
 
     return this;
 };
+
+
+//////////////////////////////////////////////////////////////////////////////
+
 
 simplex = function (Nodo) {
     /*
@@ -309,96 +303,55 @@ simplex = function (Nodo) {
     }
     else {
         //Caso tenha acontecido algum erro
+        var z = "";
         switch (r) {
             case GLP_EBADB:
-                alert("Número de variáveis básicas não é o mesmo que o número de linhas do objeto do problema. ");
+                z = "Número de variáveis básicas não é o mesmo que o número de linhas do objeto do problema. ";
                 break;
 
             case GLP_ESING:
-                alert("O modelo contém apenas uma matriz base dentro do modelo. ");
+                z = "O modelo contém apenas uma matriz base dentro do modelo. ";
                 break;
 
             case GLP_ECOND:
-                alert("Número de condição muito grande para a matriz base inicial. ");
+                z = "Número de condição muito grande para a matriz base inicial. ";
                 break;
 
             case GLP_EBOUND:
-                alert("Variáveis limitadas reais com limites incorretos. ");
+                z = "Variáveis limitadas reais com limites incorretos. ";
                 break;
 
             case GLP_EFAIL:
-                alert("A busca da resposta foi encerrada devido a falha do solver. ");
+                z = "A busca da resposta foi encerrada devido a falha do solver. ";
                 break;
 
             case GLP_EOBJLL:
-                alert("A função objetivo que era pra ser maximizada atingiu seu menor valor e continua diminuindo. ");
+                z = "A função objetivo que era pra ser maximizada atingiu seu menor valor e continua diminuindo. ";
                 break;
 
             case GLP_EOBJUL:
-                alert("A função objetivo que era pra ser minimizada atingiu seu maior valor e continua aumentando. ");
+                z = "A função objetivo que era pra ser minimizada atingiu seu maior valor e continua aumentando. ";
                 break;
 
             case GLP_EITLIM:
-                alert("A iteração do simplex excedeu o limite. ");
+                z = "A iteração do simplex excedeu o limite. ";
                 break;
 
             case GLP_ETMLIM:
-                alert("O tempo limite foi excedido. ");
+                z = "O tempo limite foi excedido. ";
                 break;
 
             case GLP_ENOPFS:
-                alert("Não tem solução viável primal. ");
+                z = "Não tem solução viável primal. ";
                 break;
 
             case GLP_ENODFS:
-                alert("Não tem solução viável dual. ");
+                z = "Não tem solução viável dual. ";
         }
 
-        return {z: "erro",
+        return {z: z,
             x: []};
     }
-//    }
-    /*else {
-     
-     var iocp = new IOCP({presolve: GLP_ON});
-     r = glp_intopt(lp, iocp);
-     if (r === 0) {
-     alert("Solução Ótima encontrada por método Branch-and-Cut");
-     z = glp_mip_obj_val(lp);
-     x = [];
-     for (var i = o; i < glp_get_num_cols(lp); i++) {
-     alert(glp_mip_col_val(lp, i));
-     }
-     }
-     else {
-     
-     switch (r) {
-     
-     case GLP_EBOUND:
-     alert("Algumas variáveis reais ou inteiras estão com os limites incorretos. ");
-     case GLP_EROOT:
-     alert("Base ideal para o problema de PL não é fornecido. ");
-     
-     case GLP_ENOPFS:
-     alert("Não tem solução viável primal. ");
-     
-     case GLP_ENODFS:
-     alert("Não tem solução viável dual, há pelo menos uma solução viável primitiva, a solução é ilimitada. ");
-     
-     case GLP_EFAIL:
-     alert("Falha do solver. ");
-     
-     case GLP_EMIPGAP:
-     alert("");
-     
-     case GLP_ETMLIM:
-     alert("");
-     
-     case GLP_ESTOP:
-     alert("");
-     }
-     }
-     }*/
 };
 
 leituraParametros = function () {
@@ -454,12 +407,12 @@ leituraParametros = function () {
 //     
     return {
         problema: 'Maximize',
-        objetivo: [4, -1],
-        restricoes: [[7, -2], [0, 1], [2, -2]],
-        relacoes: ['<=', '<=', '<='],
-        rhs: [14, 3, 3],
-        upper: ['Inf', 'Inf'],
-        lower: [0, 0]
+        objetivo: [8, 16, 20, 12, 6, 10, 4],
+        restricoes: [[3, 7, 9, 6, 3, 5, 2]],
+        relacoes: ['<='],
+        rhs: [17],
+        upper: ['Inf', 'Inf', 'Inf', 'Inf', 'Inf', 'Inf', 'Inf'],
+        lower: [0, 0, 0, 0, 0, 0, 0]
     };
 };
 
